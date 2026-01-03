7dcb910b1e7ec32c81c6a5767b2a5ad03d01b03d
Reza <behruz.mreza@gmail.com>
2026-01-03 15:59:40 +0100

    Initial Commit

---
+++ script.js
@@ -0,0 +1,75 @@
+const imgArr = ["alaska-810433_1280.jpg", "anime-8788959_1280.jpg", 
+ "atmosphere-8752835_1280.png",
+"blue-tit-8521052_1280.jpg",
+"hurricane-92968_1280.jpg",
+"lake-2896379_1280.jpg",
+"moorente-8783210_1280.jpg",
+"sea-2563389_1280.jpg",
+"snow-bunting-6781122_1280.jpg",
+"snow-leopard-cubs-8039138_1280.jpg",
+"travel-8785493_1280.jpg",
+"winter-1675197_1280.jpg"];
+
+const imgAlt = [
+"alaska-810433_1280",
+"anime-8788959_1280", 
+"atmosphere-8752835_1280",
+"blue-tit-8521052_1280",
+"hurricane-92968_1280",
+"lake-2896379_1280",
+"moorente-8783210_1280",
+"sea-2563389_1280",
+"snow-bunting-6781122_1280",
+"snow-leopard-cubs-8039138_1280",
+"travel-8785493_1280",
+"winter-1675197_1280"];
+
+const album = document.getElementById("album1");
+const dialog = document.getElementById("dialog1");
+let currentImage;
+
+function render(){
+    
+    for (let i = 0; i < imgArr.length; i++) { 
+      album.innerHTML += `<img onclick=renderDialog(${i}) src= img/${ imgArr[i] } alt= ${imgAlt[i]} >`;
+    }
+    
+}
+
+function renderDialog(i){
+  dialog.setAttribute("class","dialog");
+  dialog.innerHTML = ` 
+            <div class="dialog-head">
+              <span>${imgAlt[i]}</span>
+              <img src="img/close-icon/close.svg" alt="close-icon" onclick= closeDialog() />
+            </div>
+            <img src=img/${imgArr[i]} alt=${imgAlt[i]} />
+            <div class="dialog-footer">
+              <img src="img/close-icon/right-arrow.svg" alt="left-arrow" onclick=backward(${i}) class="rotate-180"/>
+              <span>${i + 1}/12</span>
+              <img src="img/close-icon/right-arrow.svg" alt="right-arrow" onclick=forward(${i}) />
+            </div>`;
+}
+
+function closeDialog(){
+  dialog.setAttribute("class", "dialog-hidden");
+}
+
+function backward(i){
+  if(i == 0)
+    currentImage = 11;
+  else
+    currentImage = i - 1;
+
+ renderDialog(currentImage); 
+}
+
+function forward(i){
+  if(i == 11)
+    currentImage = 0;
+  else
+    currentImage = i + 1;
+
+  renderDialog(currentImage)
+}
+
